2cc1e4c30ba42efd757978533b57b9ceaf95e178
Ishan Kulkarni <inusyd@gmail.com>
2025-04-22 15:31:53 +1000

    added a hashing function

--- Interface.py
+++ Interface.py
@@ -1,15 +1,17 @@
 import random
 print("Welcome to poker")
 
-
+cardCache = set() #set up a cache for cards
 class card: #simple class for a card
-    def __init__(self, value, suite, nomen):
+    def __init__(self, value, suite, face):
         self.value = value
         self.suite = suite
-        self.nomen = nomen
+        self.face = face
 
     def __eq__(self, other):
-        return self.value == other.value and self.suite == other.suite
+        return self.value == other.value and self.suite == other.suite #comparison function
+    def __hash__(self):
+        return hash((self.value, self.suite)) ##hashing function
 
 def generate_card(): #function for generating cards
 
@@ -18,24 +20,27 @@ def generate_card(): #function for generating cards
 
     
     card_index = random.randint(0, 51)
-    suite = suits[card_index // 13]  
-    value = values[card_index % 13]  
+    suite = suits[card_index // 13]  #assigning suites
+    value = values[card_index % 13]  ##assigning values
     return card(card_index % 13 + 1, suite, value)
 
 
+def add_card(card):
+    if card not in cardCache:  
+        cardCache.add(card)  # directly modifies the global set
     
-    
-cardCache = set() # set up a cache for cards
+
+
 c1 = generate_card()
-cardCache.add(c1) # add the first card
+add_card(c1) # add the first card
 c2 = generate_card()
-while c1.__eq__(c2):
-    c2 = generate_card() #Keep regenrating until c2 is unique
-cardCache.add(c2)
+add_card(c2)#Keep regenrating until c2 is unique
+
+#simple UI
 print("====================================")
 print("Your cards: \n")
-print(f"Card 1: {c1.nomen} of {c1.suite}")
-print(f"Card 1: {c2.nomen} of {c2.suite}")
+print(f"Card 1: {c1.face} of {c1.suite}")
+print(f"Card 1: {c2.face} of {c2.suite}")
 print("====================================")
 
 
